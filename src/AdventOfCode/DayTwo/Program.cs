370abdf3359710033afd515c43c52f7b3db5c69a
Luke Parker <lparkermg@outlook.com>
2024-12-02 21:38:48 +0000

    Advent of Code: Day 2 (#3)
    * Starts implementing second day.
    
    * Adds data and loading data.
    
    * Fixes logic in report check.
    
    * Completes day 2 challenge.

---
+++ src/AdventOfCode/DayTwo/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace DayTwo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var loadedReports = await File.ReadAllLinesAsync("./data.txt");
+            var reports = new List<Report>();
+
+            foreach(var report in loadedReports)
+            {
+                var levels = report.Split(" ").Select(i => int.Parse(i)).ToList();
+                reports.Add(new Report { Levels = levels });
+            }
+
+            var safeReports = 0;
+
+            foreach (var report in reports)
+            {
+                if (IsReportSafe(report))
+                {
+                    safeReports++;
+                }
+            }
+            Console.WriteLine($"There are {safeReports} safe reports.");
+        }
+
+        private static bool IsReportSafe(Report report, bool sub = false)
+        {
+            int[] reportLevels;
+
+            // Remove a single failure to make report valid. 
+
+            if (report.Levels.First() > report.Levels.Last())
+            {
+                // First is greater than last so we're decsending
+                reportLevels = report.Levels.Reverse().ToArray();
+
+            }
+            else
+            {
+                reportLevels = report.Levels.ToArray();
+                // Last is greater than first so we're accending
+                
+            }
+            var isSafe = true;
+            for (var i = 0; i < reportLevels.Length - 1; i++)
+            {
+                var reportItem = reportLevels[i];
+                var nextItem = reportLevels[i + 1];
+                
+                if(nextItem - reportItem > 3 || nextItem - reportItem < 1)
+                {
+                    isSafe = false;
+                }
+            }
+
+            if (!isSafe && !sub)
+            {
+                for(var i = 0; i < reportLevels.Length; i++)
+                {
+                    var levels = reportLevels.ToList();
+                    levels.RemoveAt(i);
+                    if (IsReportSafe(new Report { Levels = levels }, true))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            return isSafe;
+        }
+    }
+}
---
+++ src/AdventOfCode/DayTwo/Report.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTwo
+{
+    public class Report
+    {
+        public required IList<int> Levels { get; set; }
+    }
+}
