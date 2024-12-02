ffa1842f577890caa50f4c4981a393a3715d2070
Luke Parker <lparkermg@outlook.com>
2024-12-02 20:03:49 +0000

    Fixes logic in report check.

--- src/AdventOfCode/DayTwo/Program.cs
+++ src/AdventOfCode/DayTwo/Program.cs
@@ -9,7 +9,7 @@
 
             foreach(var report in loadedReports)
             {
-                var levels = report.Split(" ").Select(i => int.Parse(i)).ToArray();
+                var levels = report.Split(" ").Select(i => int.Parse(i)).ToList();
                 reports.Add(new Report { Levels = levels });
             }
 
@@ -46,7 +46,7 @@
                 var reportItem = reportLevels[i];
                 var nextItem = reportLevels[i + 1];
 
-                if(reportItem - nextItem > 3 || reportItem - nextItem < 1)
+                if(nextItem - reportItem > 3 || nextItem - reportItem < 1)
                 {
                     return false;
                 }
